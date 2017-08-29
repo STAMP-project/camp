a560f2f9a3fb9928d14b8fdd2e3a80c7dbded94a
vassik <anatoly.vasilevskiy@sintef.no>
2017-08-29 14:08:50 +0200

    wrapping everithing in docker container for testing

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMAGE=vassik/config-testing:latest
+APP=config-testing
+
+docker stop $APP
+docker rm $APP
+docker rmi -f $IMAGE
+docker build --rm -t $IMAGE .
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock -e MASTER_SSH_PORT=22 -e MASTER_SLAVE_USER=jenkins -e MASTER_SLAVE_PWD=jenkins -e DOCKER_GID=998 --name $APP $IMAGE
+docker exec -it $APP bash -c "su - jenkins"
