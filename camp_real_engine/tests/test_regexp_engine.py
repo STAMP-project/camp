6ad848c67638351430501926d23dc5b6b9cf92be
vassik <anatoly.vasilevskiy@sintef.no>
2018-02-15 13:28:21 +0100

    added tox setup

---
+++ camp_real_engine/tests/test_regexp_engine.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestRegExpEngine(unittest.TestCase):
+
+	def setUP(self):
+		pass
+
+	def simple_e2e_reg_exp_engine(self):
+		print "hello!!!!!!!!!!!"
+		self.assertTrue(False)
