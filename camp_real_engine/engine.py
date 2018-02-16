043a12a1926702c2ab3f8403abfc659fcd9bd88a
vassik <anatoly.vasilevskiy@sintef.no>
2018-02-16 16:29:22 +0100

    tmp commit, things are not even build...

---
+++ camp_real_engine/engine.py
@@ -0,0 +1,3 @@
+
+class Engine(object):
+	pass
---
+++ camp_real_engine/utils/parsers/substitutions/subs_textfile_parser.py
@@ -0,0 +1,15 @@
+
+
+class TextFileParser(object):
+
+	def parse(self, subst_obj):
+		pass
+
+	def get_file_content(self):
+		return ''
+
+	def get_placement_str(self):
+		return ''
+
+	def get_replacement_str(self):
+		return ''
