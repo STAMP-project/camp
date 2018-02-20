f685f5f4fd989db9aaf929cca7d55459db01ecfb
vassik <anatoly.vasilevskiy@sintef.no>
2018-02-20 12:44:52 +0100

    failed test case

--- camp_real_engine/plugins/regexp.py
+++ camp_real_engine/plugins/regexp.py
@@ -1,21 +1,22 @@
 from camp_real_engine.plugins.abstract.abc_subst_realizer import ABC_subst_realizer
-from camp_real_engine.utils.parsers.substitutions.subs_textfile_parser import TextFileParser
+from camp_real_engine.utils.parsers.substitutions.subs_textfile_parser import RegExpFileSubstParser
 
 
 
 class RegExp(ABC_subst_realizer):
 
 	def __init__(self, _parser_subst = None):
-		self.parser_subst = _parser_subst if _parser_subst else TextFileParser()
+		self.parser_subst = _parser_subst if _parser_subst else RegExpFileSubstParser()
 
 
 	def execute_subst(self, substitution):
 		self.parser_subst.parse(substitution)
 
-		content = self.parser_subst.get_file_content()
+		file_content = self.parser_subst.get_file_content()
 		placement = self.parser_subst.get_placement_str()
 		replacement = self.parser_subst.get_replacement_str()
 
 		#do substitution here
+		modified_content = ''
 
-		self.parser_subst.set_file_content(self.content)
+		self.parser_subst.set_file_content(modified_content)
