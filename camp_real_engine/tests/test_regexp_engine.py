6a1d53b12f0e1f8f852b24dd756523e267c954e4
vassik <anatoly.vasilevskiy@sintef.no>
2018-02-20 15:56:15 +0100

    we move reading and writing to another module

--- camp_real_engine/tests/test_regexp_engine.py
+++ camp_real_engine/tests/test_regexp_engine.py
@@ -1,6 +1,9 @@
+import yaml
 import unittest
 
 from camp_real_engine.plugins.regexp import RegExp
+from camp_real_engine.utils.parsers.substitutions.subs_textfile_parser import RegExpFileSubstParser
+
 
 
 class DummySubstParser(object):
@@ -8,12 +11,12 @@ class DummySubstParser(object):
 
 	def __init__(self):
 		self.file_contents = '''
-			FROM ubuntu:some_string
-			RUN cp ~/something ~/something_else
+		FROM ubuntu:some_string
+		RUN cp ~/something ~/something_else
 		'''
 	
 	def parse(self, subst_obj):
-		pass
+		return  True
 
 	def get_file_content(self):
 		return self.file_contents
@@ -31,17 +34,13 @@ class DummySubstParser(object):
 class TestRegExpEngine(unittest.TestCase):
 
 	def setUp(self):
-		pass
-
-	def test_simple_substitution(self):
-
 		self.substitution = '''
-			substituion1: 
-	          type: regexp
-	          filename: "/name/Dockerfile"
-	          placement: "some_string"
-	          replacement: "another_string"
-		'''
+substituion1: 
+ type: regexp
+ filename: "/name/Dockerfile"
+ placement: "some_string"
+ replacement: "another_string"
+ '''
 
 		self.init_file_contents = '''
 		FROM ubuntu:some_string
@@ -53,9 +52,18 @@ class TestRegExpEngine(unittest.TestCase):
 		RUN cp ~/something ~/something_else
 		'''
 
+	def test_simple_substitution(self):
 		self.dummy_subst_parser = DummySubstParser()
 		self.regexp = RegExp(_parser_subst = self.dummy_subst_parser)
 		self.regexp.execute_subst(self.substitution)
-
 		self.assertTrue(self.expected_file_contents == self.dummy_subst_parser.get_file_content(), 'failed to perform substitution')
 
+	def test_reg_exp_file_subst(self):
+		obj = yaml.load(self.substitution)
+		subs_parser = RegExpFileSubstParser()
+		is_regexp = subs_parser.parse(obj)
+		self.assertTrue(is_regexp, "faled to recognize regexp subst")
+
+		self.assertTrue("some_string" == subs_parser.get_placement_str())
+		self.assertTrue("another_string" == subs_parser.get_replacement_str())
+		self.assertTrue("/name/Dockerfile" == subs_parser.get_file_name())
