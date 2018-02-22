6a227eb3b18ffc0acdb1a56157809884b53bd93a
vassik <anatoly.vasilevskiy@sintef.no>
2018-02-22 13:41:55 +0100

    test cases to pass

--- camp_real_engine/plugins/model/realization.py
+++ camp_real_engine/plugins/model/realization.py
@@ -1,8 +1,10 @@
 
 class RegExpFileSubstNode(object):
 
-
 	def parse(self, subst_obj):
+		pass
+
+	def get_subst_label(self):
 		pass
 
 	def get_file_name(self):
@@ -13,3 +15,30 @@ class RegExpFileSubstNode(object):
 
 	def get_replacement_str(self):
 		return ''
+
+
+class VariableNode(object):
+
+	def get_variable_label(self):
+		pass
+
+
+class ValueNode(object):
+
+	def ger_value_label(self):
+		pass
+
+
+class YamlRealizationModel(object):
+
+	def parse(self, subst_obj):
+		pass
+
+	def get_variables(self):
+		pass
+
+	def get_values_by_variable(self, variable):
+		pass
+
+	def get_substitutions_by_value(self, value):
+		pass
