c060bf8ffa777ae1140a1c3a74a9180335893293
vassik <anatoly.vasilevskiy@sintef.no>
2018-02-28 16:18:11 +0100

    passed test case

--- camp_real_engine/plugins/model/product.py
+++ camp_real_engine/plugins/model/product.py
@@ -6,16 +6,79 @@ from camp_real_engine.plugins.abstract.abc_prod_data_model import ABCProductVar,
 
 
 class ProductRootNode(ABCProductRealNode, ABCProductRoot):
-	pass
+	
+	def __init__(self):
+		self.products = []
+
+	def append_product(self, prod):
+		self.products.append(prod)
+
+	def get_products(self):
+		return self.products
+
 
 class ProductNode(ABCProductRealNode, ABCProduct):
-	pass
+	
+	def __init__(self):
+		self.prod_name = None
+		self.prod_dir = None
+		self.real = None
+
+	def set_prod_name(self, _name):
+		self.prod_name = _name
+
+	def get_prod_name(self):
+		return self.prod_name
+
+	def set_prod_dir(self, _dir):
+		self.prod_dir = _dir
+
+	def get_prod_dir(self):
+		return self.prod_dir
+
+	def set_prod_real(self, _real):
+		self.real = _real
+
+	def get_prod_real(self):
+		return self.real
+
 
 class ProductRealNode(ABCProductRealNode, ABCProductReal):
-	pass
+	
+	def __init__(self):
+		self.real_path = None
+		self.prod_vars = []
+
+	def set_real_path(self, _path):
+		self.real_path = _path
+
+	def get_real_path(self):
+		return self.real_path
+
+	def append_prod_vars(self, var):
+		self.prod_vars.append(var)
+
+	def get_prod_vars(self):
+		return self.prod_vars
+
 
 class ProductVarNode(ABCProductRealNode, ABCProductVar):
-	pass
+	
+	def __init__(self):
+		self.name = None
+		self.value = None
+
+	def set_var_name(self, _name):
+		self.name = _name
+
+	def get_var_name(self):
+		return self.name
+
+	def set_var_value(self, _value):
+		self.value = _value
+
+	def get_var_value(self):
+		return self.value
 
 
 class ProductComponentFactory(object):
@@ -36,16 +99,34 @@ class ProductComponentFactory(object):
 class YamlProductVisitor(ABCProductVisitor):
 
 	def visit_product_root(self, visitee, **kwargs):
-		pass
+		yaml_root = kwargs.get("yaml_obj")
 
 	def visit_product(self, visitee, **kwargs):
-		pass
+		prod_root = kwargs.get("prod_root")
+		yaml_prod = kwargs.get("yaml_prod")
+		prod_root.append_product(visitee)
+
+		prod_name = yaml_prod.keys()[0]
+		prod_dir = yaml_prod.values()[0]['product_dir']
+		visitee.set_prod_name(prod_name)
+
+		visitee.set_prod_dir(prod_dir)
+
 
 	def visit_product_real(self, visitee, **kwargs):
-		pass
+		prod = kwargs.get('prod')
+		yaml_real = kwargs.get('yaml_real')
+
+		prod.set_prod_real(visitee)
+		visitee.set_real_path(yaml_real['path'])
 
 	def visit_product_var(self, visitee, **kwargs):
-		pass
+		real = kwargs.get("prod_real")
+		yaml_var = kwargs.get("yaml_var")
+
+		real.append_prod_vars(visitee)
+		visitee.set_var_name(yaml_var.keys()[0])
+		visitee.set_var_value(yaml_var.values()[0])
 
 
 class YamlProductPrinterVisitor(ABCProductVisitor):
@@ -70,9 +151,17 @@ class YamlProductModelBulder(object):
 		self.factory = ProductComponentFactory()
 
 	def parse(self, yaml_obj):
-		pass
-
-
-	def get_products(self):
-		pass
+		products = yaml_obj['products']
+		croot = self.factory.creat_product_root()
+		croot.accept(self.visitor, yaml_root = yaml_obj)
+		for product in products:
+			cprod = self.factory.create_product()
+			cprod.accept(self.visitor, prod_root = croot, yaml_prod = product)
+			creal = self.factory.create_product_real()
+			yaml_realization = product.values()[0]['realization']
+			creal.accept(self.visitor, prod = cprod, yaml_real = yaml_realization)
+			for variable in yaml_realization['variables']:
+				cvar = self.factory.create_product_var()
+				cvar.accept(self.visitor, prod_real = creal, yaml_var = variable)
+		return croot
 
--- camp_real_engine/tests/test_product_model.py
+++ camp_real_engine/tests/test_product_model.py
@@ -34,32 +34,32 @@ products:
 		self.assertEqual(len(products), 2)
 
 		product1, product2 = products[0], products[1]
-		self.assertEqual(product1.get_name(), "product1")
-		self.assertEqual(product2.get_name(), "product2")
+		self.assertEqual(product1.get_prod_name(), "product1")
+		self.assertEqual(product2.get_prod_name(), "product2")
 
-		self.assertEqual(product1.get_product_dir(), "camp_real_engine/tests/resources/simple_e2e_regexp/tmp/product1")
-		self.assertEqual(product2.get_product_dir(), "camp_real_engine/tests/resources/simple_e2e_regexp/tmp/product2")
+		self.assertEqual(product1.get_prod_dir(), "camp_real_engine/tests/resources/simple_e2e_regexp/tmp/product1")
+		self.assertEqual(product2.get_prod_dir(), "camp_real_engine/tests/resources/simple_e2e_regexp/tmp/product2")
 
-		realization = product1.get_product_real()
+		realization = product1.get_prod_real()
 		self.assertEqual(realization.get_real_path(), "camp_real_engine/tests/resources/simple_e2e_regexp/tmp/test_realmodel.yaml")
 
-		variables = realization.get_product_vars()
+		variables = realization.get_prod_vars()
 		self.assertEqual(len(variables), 2)
 
 		variable1, variable2 = variables[0], variables[1]
-		self.assertEqual(variable1.get_name(), "variable1")
-		self.assertEqual(variable1.get_value(), "value1")
-		self.assertEqual(variable2.get_name(), "variable1")
-		self.assertEqual(variable2.get_value(), "value2")
+		self.assertEqual(variable1.get_var_name(), "variable1")
+		self.assertEqual(variable1.get_var_value(), "value1")
+		self.assertEqual(variable2.get_var_name(), "variable1")
+		self.assertEqual(variable2.get_var_value(), "value2")
 
-		realization = product2.get_product_real()
+		realization = product2.get_prod_real()
 		self.assertEqual(realization.get_real_path(), "camp_real_engine/tests/resources/simple_e2e_regexp/tmp/test_realmodel.yaml")
 
-		variables = realization.get_product_vars()
+		variables = realization.get_prod_vars()
 		self.assertEqual(len(variables), 2)
 
 		variable1, variable2 = variables[0], variables[1]
-		self.assertEqual(variable1.get_name(), "variable1")
-		self.assertEqual(variable1.get_value(), "value1")
-		self.assertEqual(variable2.get_name(), "variable1")
-		self.assertEqual(variable2.get_value(), "value2")
+		self.assertEqual(variable1.get_var_name(), "variable1")
+		self.assertEqual(variable1.get_var_value(), "value1")
+		self.assertEqual(variable2.get_var_name(), "variable1")
+		self.assertEqual(variable2.get_var_value(), "value2")
