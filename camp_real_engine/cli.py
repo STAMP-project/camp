d19c49fbd6e685a5f8966bb2d189a203ad96fedd
vassik <anatoly.vasilevskiy@sintef.no>
2018-03-02 10:42:01 +0100

    added cli interface and test

---
+++ camp_real_engine/cli.py
@@ -0,0 +1,12 @@
+from camp_real_engine.engine import RealizationEngine
+
+
+class CLI(object):
+
+	def __init__(self, *args, **kwargs):
+		print args
+		print kwargs
+
+
+	def execute(self):
+		pass
