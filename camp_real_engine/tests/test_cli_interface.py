97b26eff681b96650a001d590432138d0b307ad9
vassik <anvasilevsky@gmail.com>
2018-03-04 16:54:23 +0100

    simple command line is done

--- camp_real_engine/tests/test_cli_interface.py
+++ camp_real_engine/tests/test_cli_interface.py
@@ -7,7 +7,7 @@ from camp_real_engine.plugins.model.product import ProductNode
 class TestCampRealizeCLI(unittest.TestCase):
 
 
-	@patch("camp_real_engine.engine.RealizationEngine")
+	@patch("camp_real_engine.cli.RealizationEngine")
 	def test_simple_cli(self, mock_RealizationEngine):
 		mock_real_engine = mock_RealizationEngine.return_value
 		mock_product = MagicMock(ProductNode)
@@ -15,8 +15,8 @@ class TestCampRealizeCLI(unittest.TestCase):
 		
 		#camp_realize.py realize /dir/some/file.yaml
 		command = ['realize', '/dir/some/file.yaml']
-		cli = CLI(command)
-		cli.execute()
+		cli = CLI()
+		cli.execute(command)
 
 		mock_real_engine.get_products.assert_called_once_with('/dir/some/file.yaml')
 		mock_real_engine.realize_product.assert_called_once_with(mock_product)
