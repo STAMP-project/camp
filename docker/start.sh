86d0a0be9a3f7cc3e2c73280d88b8fdb555c80c5
Hui Song <hui.song@sintef.no>
2018-03-27 06:10:33 +0200

    move docker files into CAMP repo

---
+++ docker/start.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+rm -rf ./workingdir/build
+rm -rf ./workingdir/out
+rm -f ./workingdir/ampimages.yml
+rm -f ./workingdir/genimages.yml
+rm -f ./workingdir/ampcompose.yml
+mkdir ./workingdir/build
+mkdir ./workingdir/out
+
+python ozepy/stamp/dockerbuild.py -d ./workingdir
+cp ./workingdir/out/ampimages.yml ./workingdir/ampimages.yml
+cp ./workingdir/out/genimages.yml ./workingdir/genimages.yml
+
+if [ -f ./workingdir/composite.yml ]
+then
+    python ozepy/stamp/dockercompose.py -d ./workingdir
+    cp ./workingdir/out/ampcompose.yml ./workingdir/ampcompose.yml
+fi
+
+python camp/src/dockerfilegen.py -i ./workingdir/genimages.yml
+
+if [ -f ./workingdir/composite.yml ]
+then
+    python camp/src/composegen.py -i ./workingdir/ampcompose.yml
+fi
+
+if [ -f ./workingdir/resolmodel.yml ]
+then
+    cp ./working/out/ampcompose.yml ./workingdir/ampcompose.yml
+    #python camp-realize/camp_real_engine/rcamp.py -i workingdir/resolmodel.yml
+    if [ -x "$(command -v rcamp)" ]; then
+    	rcamp realize ./workingdir/resolmodel.yml
+    else
+    	echo "rcamp is not installed, variables will not be materialized"
+    fi 
+fi
+
+chmod -R a+rw . 
+
+echo ""
+echo "===========Searching finished.========="
+echo "Build all the generated images by: cd <your_dir>/build && bash ./build.sh"
+echo "Launch one of the configurations by: docker-compose <your_dir>/docker-compose/docker-comopose-<number>.yml up"
