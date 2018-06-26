dfe13e6767d57d3f8c6af935065af64161150119
Fernando <fernando.mendez@atos.net>
2018-06-26 05:21:04 -0400

    Updated script citygo

---
+++ samples/stamp/atos/citygo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+#This script download CityGo app from Atos Repository
+
+rm -rf ./repo
+#Clone citygo from ARI gitlab
+git clone https://gitlab.atosresearch.eu/ari/stamp_docker_citygoApp.git
+
+#Copy required files to deploy citygo
+mkdir ./repo
+cp -R stamp_docker_citygoApp/repo/Showcase repo/
+cp -R stamp_docker_citygoApp/repo/Postgres repo/
+cp  stamp_docker_citygoApp/docker-compose-camp/docker-compose.yml docker-compose/
+
+#Remove citygo ARI gitlab
+rm -rf stamp_docker_citygoApp/
+
+
+
+
