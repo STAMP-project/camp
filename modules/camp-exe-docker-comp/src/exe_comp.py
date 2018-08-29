add9b4866a9aae06b36a55d366369e9cd1e20709
vassik <anatoly.vasilevskiy@sintef.no>
2018-08-29 12:38:25 +0200

    adding initial code for camp exe for docker compose files

---
+++ modules/camp-exe-docker-comp/src/exe_comp.py
@@ -0,0 +1,13 @@
+import sys, getopt
+
+
+def main(argv):
+	pass
+
+
+def exe_comp_cli(argv):
+	main(sys.argv[1:])
+
+
+if __name__ = "__main__":
+	main(sys.argv[1:])
---
+++ modules/camp-exe-docker-comp/tests/test_simple_exe.py
@@ -0,0 +1,6 @@
+import unittest
+
+class TestSimpleComp(unittest.TestCase):
+
+	def test_docker_compose_exe(self):
+		pass
