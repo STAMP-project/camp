e27e22fa4fc987dafb5315db77585c31470711f1
vassik <anatoly.vasilevskiy@sintef.no>
2018-08-31 11:14:39 +0200

    adding a simple test case to pass

--- modules/camp-exe-docker-comp/core/parser/parsers.py
+++ modules/camp-exe-docker-comp/core/parser/parsers.py
@@ -14,7 +14,7 @@ class ConfigINIParser(object):
 	def __init__(self, _visitor=None):
 		self.visitor = _visitor 
 
-	def parse(self, file_locat):
+	def parse(self, _file):
 		_config = ConfigParser.RawConfigParser()
 
 		return ConfigRoot()
--- modules/camp-exe-docker-comp/tests/test_simple_exe.py
+++ modules/camp-exe-docker-comp/tests/test_simple_exe.py
@@ -1,9 +1,26 @@
 import unittest
 
+from core.parser.parsers import ConfigINIParser
+
+
 class TestSimpleComp(unittest.TestCase):
 
 	def test_docker_compose_exe(self):
 		pass
 
-	def test_config_parsing(self):
-		pass
+	def test_config_ini_parsing(self):
+		file = "file/location"
+		parser = ConfigINIParser()
+		config = parser.parse(file)
+
+		build_script = config.get_build_script()
+		self.assertEqual(build_script, 'build/build.sh')
+
+		compose_files = config.get_compose_files()
+		self.assertEqual(len(compose_files), 2)
+
+		exp_script = config.get_exp_script()
+		self.assertEqual(exp_script, 'experiment/executeExp.sh')
+
+		exp_params = config.get_exp_params()
+		self.assertEqual(exp_params, 'param1 param2')
