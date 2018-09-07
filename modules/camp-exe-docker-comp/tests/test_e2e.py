8ef6ce9097d7bf581aad7e056d0cd1f88596a573
vassik <anatoly.vasilevskiy@sintef.no>
2018-09-07 10:06:29 +0200

    adding few more test on script execution with error or without

--- modules/camp-exe-docker-comp/tests/test_e2e.py
+++ modules/camp-exe-docker-comp/tests/test_e2e.py
@@ -22,4 +22,32 @@ class TestE2E(unittest.TestCase):
 		self.assertTrue(result)
 		self.assertEqual(len(commads), 2)
 		print commads[1].logs
-		
+
+	def test_script_exe_e2e(self):
+		script_obj = Script('tests/resources/scripts/script.sh', ['param1'])
+		result = script_obj.run()
+		commands = script_obj.get_result()
+
+		self.assertTrue(result)
+		self.assertEqual(len(commands), 1)
+		self.assertEqual(commands[0].status, 0)
+
+		stdout = commands[0].logs['stdout']
+		stderr = commands[0].logs['stderr']
+		expected_stdout = './script.sh param1'
+		self.assertEqual(stdout.strip(), expected_stdout)
+		self.assertEqual(stderr.strip(), '')
+
+	def test_script_exe_failure_e2e(self):
+		script_obj = Script('tests/resources/scripts/script_err.sh')
+		result = script_obj.run()
+		commands = script_obj.get_result()
+
+		self.assertFalse(result)
+		self.assertEqual(len(commands), 1)
+		self.assertTrue(commands[0].status != 0)
+
+		stdout = commands[0].logs['stdout']
+		stderr = commands[0].logs['stderr']
+		self.assertEqual(stdout.strip(), '')
+		self.assertTrue(stderr.strip() != 0)
