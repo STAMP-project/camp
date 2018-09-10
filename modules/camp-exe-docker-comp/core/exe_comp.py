d2cf4f36d842be7113f67f87b07b2cdc2a14edfb
vassik <anatoly.vasilevskiy@sintef.no>
2018-09-10 11:23:25 +0200

    fixed issues with cli

--- modules/camp-exe-docker-comp/core/exe_comp.py
+++ modules/camp-exe-docker-comp/core/exe_comp.py
@@ -7,9 +7,9 @@ def main(commands):
     cli.execute(commands)
 
 
-def exe_comp_cli(argv):
+def exe_comp_cli():
 	main(sys.argv[1:])
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
 	main(sys.argv[1:])
