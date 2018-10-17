56ff4803971d39d87037a96ade0096e481a12d3d
Franck Chauvel <franck.chauvel@gmail.com>
2018-10-17 13:08:07 +0200

    Deleting module 'camp-exe-docker-comp' now integrated as the 'camp execute' command

--- modules/camp-exe-docker-comp/core/utility/tools.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-def output_message(message):
-	sys.stdout.write(message)
-
-def output_message_error(message):
-	sys.stderr.write(message)
--- modules/camp-exe-docker-comp/examples/simple/scripts/exp_composetest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Waiting for 5 sec to set up deployment"
-sleep 5
-RESP=$(curl 127.0.0.1:5000)
-#RESP='Hello World! I have been called.'
-echo "$RESP param1:$1 param2:$2"
