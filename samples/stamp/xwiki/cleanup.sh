6eb15c7e0d15c8987ac56cccab30426aee027360
Franck Chauvel <franck.chauvel@gmail.com>
2018-11-19 11:07:39 +0100

    Cleaning example so that they match the new input format

--- samples/stamp/xwiki/cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Cleaning up..."
-rm -rf ./build
-rm -rf ./out
-rm -f ./ampimages.yml
-rm -f ./genimages.yml
-rm -f ./ampcompose.yml
-rm -rf ./compose*
--- samples/stamp/xwiki/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Removing containers"
-docker rm -f xwiki-mysql-db
-docker rm -f xwiki-smoke-client-test
-docker rm -f xwiki-web
-docker rm -f xwiki-postgres-db
