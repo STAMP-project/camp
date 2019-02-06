7531910aadd8d3703b6a6db6c739c6e886193d1d
Mohamed Boussaa <mohamedboussaa3@gmail.com>
2019-02-06 17:27:46 +0100

    add mysql coonector jar in addons

--- samples/stamp/activeeon/template/proactive/change-proactive-db-config-mysql.sh
+++ samples/stamp/activeeon/template/proactive/change-proactive-db-config-mysql.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # add jdbc mysql connector 
-RUN  curl -LO https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.15.zip
-RUN  unzip mysql-connector-java-8.0.15.zip -d connectorj
-RUN  cp connectorj/mysql-connector-java-8.0.15/mysql-connector-java-8.0.15.jar addons/
-RUN ls addons/
+curl -LO https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.15.zip
+unzip mysql-connector-java-8.0.15.zip -d connectorj
+cp connectorj/mysql-connector-java-8.0.15/mysql-connector-java-8.0.15.jar addons/
+ls addons/
 
 # HashMap for scheduler config
 # ~/config/scheduler/database.properties
