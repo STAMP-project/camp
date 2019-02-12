a3f25aa594ec36f61a5995f965d2cc5d794690e3
Mohamed BOUSSAA <mohamedboussaa3@gmail.com>
2019-02-12 14:27:04 +0100

    Set up camp to generate seperately PA-Postgres and PA-MySQL configs (#2)
    * set up camp to generate seperately PA-Postgress and PA-MySQL configs
    
    * remove .DS_Store
    
    * remove Dockerfile comments
    
    * remove comments

--- samples/stamp/activeeon/template/proactive/change-proactive-db-config-postgres.sh
+++ samples/stamp/activeeon/template/proactive/change-proactive-db-config-postgres.sh
@@ -1,69 +1,66 @@
 #!/bin/bash
-# get HOST_ADDRESS for my SQL Config
+# get HOST_ADDRESS for my postgres Config
 # HOST_ADDRESS="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
-
 HOST_ADDRESS="<YOUR_ADDRESS>"
-# use this command to get your IP address: ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
-echo "myhost"$HOST_ADDRESS
-# add jdbc mysql connector 
-curl -LO https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.15.zip
-unzip mysql-connector-java-8.0.15.zip -d connectorj
-cp connectorj/mysql-connector-java-8.0.15/mysql-connector-java-8.0.15.jar addons/
-ls addons/
+
+# add jdbc postgres connector 
+curl -LO https://jdbc.postgresql.org/download/postgresql-42.2.5.jar
+cp postgresql-42.2.5.jar addons/
 
 # HashMap for scheduler config
 # ~/config/scheduler/database.properties
 declare -A scheduler
-scheduler["hibernate.connection.driver_class"]="com.mysql.cj.jdbc.Driver"
-scheduler["hibernate.connection.url"]="jdbc:mysql://"$HOST_ADDRESS":3306/scheduler?verifyServerCertificate=false\&useSSL=false\&useLegacyDatetimeCode=false\&serverTimezone=UTC"
-scheduler["hibernate.dialect"]="org.hibernate.dialect.MySQL5InnoDBDialect"
+scheduler["hibernate.connection.driver_class"]="org.postgresql.Driver"
+scheduler["hibernate.connection.url"]="jdbc:postgresql://"$HOST_ADDRESS":5432/scheduler"
+scheduler["hibernate.dialect"]="org.hibernate.dialect.PostgreSQLDialect"
 scheduler["hibernate.connection.username"]="scheduler"
 scheduler["hibernate.connection.password"]="scheduler"
 
 # HashMap for rm config
 # ~/config/rm/database.properties
 declare -A rm
-rm["hibernate.connection.driver_class"]="com.mysql.cj.jdbc.Driver"
-rm["hibernate.connection.url"]="jdbc:mysql://"$HOST_ADDRESS":3306/rm?verifyServerCertificate=false\&useSSL=false\&useLegacyDatetimeCode=false\&serverTimezone=UTC"
-rm["hibernate.dialect"]="org.hibernate.dialect.MySQL5InnoDBDialect"
-rm["hibernate.connection.username"]="rm"
-rm["hibernate.connection.password"]="rm"
+rm["hibernate.connection.driver_class"]="org.postgresql.Driver"
+rm["hibernate.connection.url"]="jdbc:postgresql://"$HOST_ADDRESS":5432/scheduler"
+rm["hibernate.dialect"]="org.hibernate.dialect.PostgreSQLDialect"
+rm["hibernate.connection.username"]="scheduler"
+rm["hibernate.connection.password"]="scheduler"
 
 # HashMap for catalog config
 # ~/dist/war/catalog/WEB-INF/classes/application.properties
 declare -A catalog
-catalog["spring.datasource.url"]="jdbc:mysql://"$HOST_ADDRESS":3306/catalog?verifyServerCertificate=false\&useSSL=false\&useLegacyDatetimeCode=false\&serverTimezone=UTC"
-catalog["spring.datasource.username"]="catalog"
-catalog["spring.datasource.password"]="catalog"
-catalog["spring.datasource.driverClassName"]="com.mysql.cj.jdbc.Driver"
-catalog["spring.jpa.database-platform"]="org.hibernate.dialect.MySQL5InnoDBDialect"
+catalog["spring.datasource.url"]="jdbc:postgresql://"$HOST_ADDRESS":5432/scheduler"
+catalog["spring.datasource.username"]="scheduler"
+catalog["spring.datasource.password"]="scheduler"
+catalog["spring.datasource.driverClassName"]="org.postgresql.Driver"
+catalog["spring.datasource.driver-class-name"]="org.postgresql.Driver"
+catalog["spring.jpa.database-platform"]="org.hibernate.dialect.PostgreSQLDialect"
 
 # HashMap for scheduling config
 # ~/dist/war/scheduling-api/WEB-INF/classes/application.properties
 declare -A scheduling
-scheduling["spring.datasource.url"]="jdbc:mysql://"$HOST_ADDRESS":3306/scheduler?verifyServerCertificate=false\&useSSL=false\&useLegacyDatetimeCode=false\&serverTimezone=UTC"
+scheduling["spring.datasource.url"]="jdbc:postgresql://"$HOST_ADDRESS":5432/scheduler"
 scheduling["spring.datasource.username"]="scheduler"
 scheduling["spring.datasource.password"]="scheduler"
-scheduling["spring.datasource.driver-class-name"]="com.mysql.cj.jdbc.Driver"
-scheduling["spring.jpa.database-platform"]="org.hibernate.dialect.MySQL5InnoDBDialect"
+scheduling["spring.datasource.driver-class-name"]="org.postgresql.Driver"
+scheduling["spring.jpa.database-platform"]="org.hibernate.dialect.PostgreSQLDialect"
 
 # HashMap for job planner config
 # ~/dist/war/job-planner/WEB-INF/classes/application.properties
 declare -A jp
-jp["spring.datasource.url"]="jdbc:mysql://"$HOST_ADDRESS":3306/scheduler?verifyServerCertificate=false\&useSSL=false\&useLegacyDatetimeCode=false\&serverTimezone=UTC"
+jp["spring.datasource.url"]="jdbc:postgresql://"$HOST_ADDRESS":5432/scheduler"
 jp["spring.datasource.username"]="scheduler"
 jp["spring.datasource.password"]="scheduler"
-jp["spring.datasource.driver-class-name"]="com.mysql.cj.jdbc.Driver"
-jp["spring.jpa.database-platform"]="org.hibernate.dialect.MySQL5InnoDBDialect"
+jp["spring.datasource.driver-class-name"]="org.postgresql.Driver"
+jp["spring.jpa.database-platform"]="org.hibernate.dialect.PostgreSQLDialect"
 
 # HashMap for pcw config
 # ~/dist/war/proactive-cloud-watch/WEB-INF/classes/application.properties
 declare -A pcw
-pcw["spring.datasource.url"]="jdbc:mysql://"$HOST_ADDRESS":3306/scheduler?verifyServerCertificate=false\&useSSL=false\&useLegacyDatetimeCode=false\&serverTimezone=UTC"
+pcw["spring.datasource.url"]="jdbc:postgresql://"$HOST_ADDRESS":5432/scheduler"
 pcw["spring.datasource.username"]="scheduler"
 pcw["spring.datasource.password"]="scheduler"
-pcw["spring.datasource.driver-class-name"]="com.mysql.cj.jdbc.Driver"
-pcw["spring.jpa.database-platform"]="org.hibernate.dialect.MySQL5InnoDBDialect"
+pcw["spring.datasource.driver-class-name"]="org.postgresql.Driver"
+pcw["spring.jpa.database-platform"]="org.hibernate.dialect.PostgreSQLDialect"
 
 # Change config of dist/war/catalog/WEB-INF/classes/application.properties
 for key in ${!scheduler[@]}
@@ -107,7 +104,3 @@ grep -q '^'$key'' dist/war/proactive-cloud-watch/WEB-INF/classes/application.pro
 $key"="$(sed 's/\\//g' <<< "${pcw[$key]}")" >> dist/war/proactive-cloud-watch/WEB-INF/classes/application.properties
 done
 
-#add jdbc mysql connector 
-#curl -LO https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.15.zip
-#unzip mysql-connector-java-8.0.15.zip -d connectorj
-#cp connectorj/mysql-connector-java-8.0.15/mysql-connector-java-8.0.15.jar addons/
--- samples/stamp/activeeon/template/proactivePG/activeeon_proactive_server_entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd /root/proactive/activeeon_enterprise-pca_server-linux-x64-8.4.0/bin
-./proactive-server
