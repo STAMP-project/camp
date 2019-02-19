cf9efe424864d50dcd77e6c153509ae5d3d97a07
Mohamed BOUSSAA <mohamedboussaa3@gmail.com>
2019-02-19 11:43:47 +0100

    Replace PA 8.4.0 release by snapshot version and update readme (#4)
    * update
    
    * replace PA 8.4.0 reslease by snapshot version
    
    * update readme
    
    * update readme

--- samples/stamp/activeeon/template/proactivePG/activeeon_proactive_server_entrypoint.sh
+++ samples/stamp/activeeon/template/proactivePG/activeeon_proactive_server_entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-cd /root/proactive/activeeon_enterprise-pca_server-linux-x64-8.4.0/bin
+cd /root/proactive/activeeon_enterprise-pca_server-linux-x64-8.5.0-SNAPSHOT/bin
 ./proactive-server
