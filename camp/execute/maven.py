708df41cd56c1d0f516ef8162e39a454e9ea1060
Franck Chauvel <franck.chauvel@gmail.com>
2019-03-18 15:23:39 +0100

    Parsing JUnit XML

--- camp/execute/maven.py
+++ camp/execute/maven.py
@@ -9,7 +9,11 @@
 #
 
 
-from camp.execute.commons import Executor, TestResults
+from camp.execute.commons import Executor, SuccessfulTest, FailedTest, ErroneousTest, \
+    TestSuite, TestResults
+
+from xml.etree.ElementTree import fromstring
+
 
 
 
@@ -29,3 +33,60 @@ class MavenExecutor(Executor):
 
     def _collect_results(self, path):
         return TestResults(path, 3, 3, 4)
+
+
+
+
+
+class JUnitXMLReader:
+    """Name of the XML elements and attributes in the JUnitXML"""
+
+    ERROR = "error"
+    FAILURE = "failure"
+    MESSAGE = "message"
+    NAME = "name"
+    TEST_CASE = "testcase"
+    TEST_SUITE = "testsuite"
+
+
+    def _extract_from_text(self, xml):
+        root = fromstring(xml)
+        if root.tag == "testsuite":
+            return self._extract_test_suite_from(root)
+
+        if root.tag == "testcase":
+            return self._extract_test_from(root)
+
+        raise JUnitXMLElementNotSupported(root.tag)
+
+
+    def _extract_test_suite_from(self, element):
+        tests = []
+        identifier = element.get(self.NAME)
+        for each in element.findall(self.TEST_CASE):
+            test = self._extract_test_from(each)
+            tests.append(test)
+        return TestSuite(identifier, *tests)
+
+
+    def _extract_test_from(self, element):
+        identifier = element.get(self.NAME)
+
+        failure = element.find(self.FAILURE)
+        if failure is not None:
+            message = failure.get(self.MESSAGE)
+            return FailedTest(identifier, message)
+
+        error = element.find(self.ERROR)
+        if error is not None:
+            message = error.get(self.MESSAGE)
+            return ErroneousTest(identifier, message)
+
+        return SuccessfulTest(identifier)
+
+
+
+class JUnitXMLElementNotSupported(Exception):
+
+    def __init__(self, element):
+        self._element = element
--- tests/execute/test_maven.py
+++ tests/execute/test_maven.py
@@ -10,8 +10,10 @@
 
 
 
-from camp.execute.commons import SimulatedShell
-from camp.execute.maven import MavenExecutor
+from camp.execute.commons import SimulatedShell, FailedTest, SuccessfulTest, \
+    TestSuite, ErroneousTest
+from camp.execute.maven import MavenExecutor, JUnitXMLReader, \
+    JUnitXMLElementNotSupported
 
 from StringIO import StringIO
 
@@ -36,3 +38,73 @@ class TheMavenExecutorShould(TestCase):
         self.assertIn("bash build_images.sh", self._log.getvalue())
         self.assertIn("docker-compose up -d", self._log.getvalue())
         self.assertIn("docker-compose exec -it tests mvn test", self._log.getvalue())
+
+
+
+class TheJUnitXMLReaderShould(TestCase):
+
+    def setUp(self):
+        self._reader = JUnitXMLReader()
+
+
+    def test_raise_exception_when_given_invalid_XML(self):
+        xml = "<invalid-element name=\"foo\"/>"
+
+        with self.assertRaises(JUnitXMLElementNotSupported):
+            test = self._reader._extract_from_text(xml)
+
+
+
+    def test_extract_a_successful_test_from_XML(self):
+        xml = "<testcase name=\"foo\"></testcase>"
+
+        test = self._reader._extract_from_text(xml)
+
+        self.assertIsInstance(test, SuccessfulTest)
+        self.assertEquals("foo", test.identifier)
+
+
+    def test_extract_a_failed_test_from_XML(self):
+        xml = ("<testcase name=\"foo\">"
+               "  <failure message=\"Something happened!\"/>"
+               "</testcase>")
+
+        test = self._reader._extract_from_text(xml)
+
+        self.assertIsInstance(test, FailedTest)
+        self.assertEquals("foo", test.identifier)
+        self.assertEquals("Something happened!", test.failure)
+
+
+    def test_extract_en_erroneous_test(self):
+        xml = ("<testcase name=\"foo\">"
+               "  <error message=\"An error occurred!\"/>"
+               "</testcase>")
+
+        test = self._reader._extract_from_text(xml)
+
+        self.assertIsInstance(test, ErroneousTest)
+        self.assertEquals("foo", test.identifier)
+        self.assertEquals("An error occurred!", test.error)
+
+
+    def test_extract_a_test_suite(self):
+        xml = (
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+            "<testsuite name=\"org.samples.mysuite\">"
+            "  <testcase name=\"Test 1\"/>"
+            "  <testcase name=\"Test 2\"/>"
+            "  <testcase name=\"Test 3\">"
+            "    <failure message=\"Something happened!\"/>"
+            "  </testcase>"
+            "</testsuite>"
+        )
+
+        test = self._reader._extract_from_text(xml)
+
+        self.assertIsInstance(test, TestSuite)
+        self.assertEquals("org.samples.mysuite", test.identifier)
+        self.assertEquals(3, test.run_test_count)
+        self.assertEquals(2, test.passed_test_count)
+        self.assertEquals(1, test.failed_test_count)
+        self.assertEquals(0, test.erroneous_test_count)
