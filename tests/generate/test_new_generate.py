5aa888ac77e3111c00a73646a89d877f5ff25a37
Franck Chauvel <franck.chauvel@gmail.com>
2019-04-03 13:22:27 +0200

    Fix build + all tests pass on Python 2.7

--- tests/generate/test_new_generate.py
+++ tests/generate/test_new_generate.py
@@ -13,7 +13,7 @@
 from camp.codecs.yaml import YAML
 from camp.generate import Z3Problem
 
-from io import StringIO
+from io import BytesIO
 
 from unittest import TestCase
 
@@ -26,7 +26,7 @@ class VariablesAreAssigned(TestCase):
 
 
     def assert_variables_values(self, text, configuration_count, expected_values):
-        model = self._yaml.load_model_from(StringIO(text))
+        model = self._yaml.load_model_from(BytesIO(text))
 
         self.assertFalse(self._yaml.warnings, "\n".join(str(each) for each in self._yaml.warnings))
 
