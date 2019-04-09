c5a8e4c0f502ba1cd652f0d68e13e2ba427cc0b3
Franck Chauvel <franck.chauvel@gmail.com>
2019-04-09 17:32:31 +0200

    Drop Tecor

--- modules/tecor/kill.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IMAGE=vassik/config-testing:latest
-APP=config-testing
-
-docker stop $APP
-docker rm $APP
-docker rmi -f $IMAGE
--- modules/tecor/testframework/docs/demos/icst2018/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APP_NAME=tecor-demo
-IMAGE_NAME=vassik/tecor-demo:latest
-docker stop $APP_NAME
-docker rm $APP_NAME
-docker rmi $IMAGE_NAME
-docker run -dit -p 8071:80 --name $APP_NAME $IMAGE_NAME
