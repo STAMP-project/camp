9d5a4582ff00caa4e8ddac7f5592d8988c17b78d
Franck Chauvel <franck.chauvel@gmail.com>
2019-05-05 14:59:33 +0200

    Add ResourceSelection entity

--- tests/entities/test_model.py
+++ tests/entities/test_model.py
@@ -12,7 +12,7 @@
 
 from __future__ import unicode_literals
 
-from camp.entities.model import TestSettings
+from camp.entities.model import TestSettings, ResourceSelection
 
 from unittest import TestCase
 
@@ -52,3 +52,39 @@ class TheTestSettingsShould(TestCase):
     def test_expose_a_pattern_to_detect_test_report(self):
         self.assertEqual(self._report_pattern,
                          self._settings.report_pattern)
+
+
+class ResourceSelectionsShould(TestCase):
+
+
+    def setUp(self):
+        self._resource = "whatever.txt"
+        self._selection = ResourceSelection(self._resource)
+
+
+    def test_expose_the_selected_resource(self):
+        self.assertEqual(self._resource, self._selection.resource)
+
+
+    def test_equal_itself(self):
+        self.assertEqual(self._selection, self._selection)
+
+
+    def test_differ_from_a_different_selection(self):
+        other = ResourceSelection("something different")
+        self.assertNotEqual(self._selection, other)
+
+
+    def test_equal_another_identical_selection(self):
+        other = ResourceSelection(self._resource)
+        self.assertEqual(self._selection, other)
+
+
+    def test_have_the_same_hash_than_an_identical_resource(self):
+        other = ResourceSelection(self._resource)
+        self.assertEqual(hash(self._selection), hash(other))
+
+
+    def test_have_a_different_hash_from_an_identical_resource(self):
+        other = ResourceSelection("whatever different")
+        self.assertNotEqual(hash(self._selection), hash(other))
