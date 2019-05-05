c4e8dbf969d108532670566a1d1cf79698ed4782
brice-morin <brice.morin1@gmail.com>
2019-05-05 17:07:51 +0200

    small improvement to atom xp

--- samples/atom/build_all.sh
+++ samples/atom/build_all.sh
@@ -7,4 +7,5 @@ for d in */ ; do
     cd $BASEDIR/out/$d/images/atom_0
     (docker build -t bmorin/atom-${d::-1} .) 2>&1 | tee ../../test.log
     docker rmi -f bmorin/atom-${d::-1}
+    sed -n -e '/passing/,$p' ../../test.log > ../../test_results.log
 done
