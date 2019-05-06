44a87f77b880a22d6db12cf1ce4af62ba4a95447
brice-morin <brice.morin1@gmail.com>
2019-05-06 22:45:11 +0200

    quick and dirty workaround for #57

--- samples/purescript/build_all.sh
+++ samples/purescript/build_all.sh
@@ -5,6 +5,10 @@ cd out
 for d in */ ; do
     echo $d
     cd $BASEDIR/out/$d/images/purescript_0
+
+    #See https://github.com/STAMP-project/camp/issues/57
+    sed -i '/stability: experimental/Q' src/package.yaml
+
     (docker build -t bmorin/purescript-${d::-1} .) 2>&1 | tee ../../test.log
     docker rm -f $(docker ps -aq)
     docker rmi -f bmorin/purescript-${d::-1}
