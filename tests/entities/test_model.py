0137f55709d42e73761169baec581ded631f8c36
Franck Chauvel <franck.chauvel@gmail.com>
2019-05-06 12:18:54 +0200

    Add entity RenameResource

--- tests/entities/test_model.py
+++ tests/entities/test_model.py
@@ -12,7 +12,7 @@
 
 from __future__ import unicode_literals
 
-from camp.entities.model import TestSettings, ResourceSelection
+from camp.entities.model import TestSettings, ResourceSelection, RenameResource
 
 from unittest import TestCase
 
@@ -88,3 +88,43 @@ class ResourceSelectionsShould(TestCase):
     def test_have_a_different_hash_from_an_identical_resource(self):
         other = ResourceSelection("whatever different")
         self.assertNotEqual(hash(self._selection), hash(other))
+
+
+
+class RenameResourceShould(TestCase):
+
+
+    def setUp(self):
+        self._resource = "dir/resource.txt"
+        self._new_name = "better_name.txt"
+        self._action = RenameResource(self._resource, self._new_name)
+
+    def test_expose_the_resource(self):
+        self.assertEqual(self._action.resource, self._resource)
+
+    def test_expose_its_new_name(self):
+        self.assertEqual(self._action.new_name, self._new_name)
+
+
+    def test_equal_itself(self):
+        self.assertEqual(self._action, self._action)
+
+
+    def test_differ_from_a_different_selection(self):
+        other = RenameResource("something different", "something")
+        self.assertNotEqual(self._action, other)
+
+
+    def test_equal_another_identical_selection(self):
+        other = RenameResource(self._resource, self._new_name)
+        self.assertEqual(self._action, other)
+
+
+    def test_have_the_same_hash_than_an_identical_resource(self):
+        other = RenameResource(self._resource, self._new_name)
+        self.assertEqual(hash(self._action), hash(other))
+
+
+    # def test_have_a_different_hash_from_an_identical_resource(self):
+    #     other = ResourceSelection("whatever different")
+    #     self.assertNotEqual(hash(self._selection), hash(other))
