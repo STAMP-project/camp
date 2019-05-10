de83b95b83dc3a1b675aefc4b22bb270bcd7706f
Daniele Gagliardi <daniele.gagliardi@eng.it>
2019-05-10 18:03:56 +0200

    Add support to JMeter CSV report: preliminary version

---
+++ tests/execute/reporting/test_jmeter.py
@@ -0,0 +1,30 @@
+#
+# CAMP
+#
+# Copyright (C) 2017 -- 2019 SINTEF Digital
+# All rights reserved.
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE file for details.
+#
+
+
+
+from camp.entities.report import FailedTest, SuccessfulTest, \
+    TestSuite, ErroneousTest
+from camp.execute.reporting.jmeter import JMeterCSVReader, \
+    JMeterCSVInvalidReport
+
+from unittest import TestCase
+
+class TheJMeterCSVReaderShould(TestCase):
+
+    def setUp(self):
+        self._reader = JMeterCSVReader()
+
+
+    def test_raise_exception_when_given_invalid_CSV_report(self):
+        csv_report = "# Samples,Average,Median,90\% Line,95\% Line,99\% Line,Min,Max,Throughput,Received KB\/sec,Sent KB\/sec"
+
+        with self.assertRaises(JMeterCSVInvalidReport):
+            self._reader._extract_from_text(csv_report)
