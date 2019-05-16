fc9fb75f66c68ffd89d0d57f978192d7d5db4255
brice-morin <brice.morin1@gmail.com>
2019-05-16 13:50:18 +0200

    complementary sphinx xp

--- samples/sphinx/xp3.sh
+++ samples/sphinx/xp3.sh
@@ -3,17 +3,17 @@
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 rm -rf $BASEDIR/out
-rm -rf $BASEDIR/out_all_refined
+rm -rf $BASEDIR/out_all_refined2
 
 docker pull fchauvel/camp
 
 echo "XP all (refined)"
 echo "------"
-cp camp.yaml.2 camp.yaml
+cp camp.yaml.3 camp.yaml
 docker run -v $BASEDIR:/camp fchauvel/camp camp generate --all -d .
 docker run -v $BASEDIR:/camp fchauvel/camp camp realize -d .
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $BASEDIR:/camp fchauvel/camp camp execute -d .
-mv $BASEDIR/out $BASEDIR/out_all_refined
+mv $BASEDIR/out $BASEDIR/out_all_refined2
 rm camp.yaml
 
 docker rm -f $(docker ps -aq)
