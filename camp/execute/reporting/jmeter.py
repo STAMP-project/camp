4d7ee4bbad6a89620595eae864f3153f8d8d9897
Daniele Gagliardi <daniele.gagliardi@eng.it>
2019-05-20 18:14:06 +0200

    Fixed wrong usage of DictReader. Now Jmeter CVS REader is able to read content from CSV file passed as file or simple String

--- camp/execute/reporting/jmeter.py
+++ camp/execute/reporting/jmeter.py
@@ -24,51 +24,55 @@ class JMeterCSVReader(ReportReader):
     NUM_OF_SAMPLES = "# Samples"
     AVG_RESP_TIME = "Average"
     MEDIAN_RESP_TIME = "Median"
-    PERCENTILE_RESP_TIME_90 = "90\% Line"
-    PERCENTILE_RESP_TIME_95 = "95\% Line"
-    PERCENTILE_RESP_TIME_99 = "99\% Line"
+    PERCENTILE_RESP_TIME_90 = "90% Line"
+    PERCENTILE_RESP_TIME_95 = "95% Line"
+    PERCENTILE_RESP_TIME_99 = "99% Line"
     MIN_RESP_TIME = "Min"
     MAX_RESP_TIME = "Max"
-    ERR_PERCENTAGE = "Error \%"
+    ERR_PERCENTAGE = "Error %"
     THROUGHPUT = "Throughput"
-    RECEIVED_BYTES = "Received KB\/sec"
-    SENT_BYTES = "Sent KB\/sec"
+    RECEIVED_BYTES = "Received KB/sec"
+    SENT_BYTES = "Sent KB/sec"
 
 
     def _extract_from_text(self, csvfile):
 
     	# TODO: currently it doesn't take into account all JMeter report fields
 
-		tests = []
+        tests = []
 
-		reader = csv.DictReader(csvfile)
+        reader = csv.DictReader(csvfile)
 
-		if reader.line_num == 0:
-			raise JMeterCSVInvalidReport("Empty JMeter Report")
+        if self.URL_LABEL not in reader.fieldnames:
+            raise JMeterCSVInvalidReport("No URL Label present in JMeter Report")
 
-		if self.URL_LABEL not in reader.fieldnames:
-			raise JMeterCSVInvalidReport("No URL Label present in JMeter Report")
+        if self.ERR_PERCENTAGE not in reader.fieldnames:
+            raise JMeterCSVInvalidReport("No error percentage value present in JMeter Report")
 
-		if self.ERR_PERCENTAGE not in reader.fieldnames:
-			raise JMeterCSVInvalidReport("No error percentage value present in JMeter Report")
+        for row in reader:
+            test = self._extract_test_from(row)
+            tests.append(test)
+        
+        if reader.line_num == 1:
+            raise JMeterCSVInvalidReport("Empty JMeter Report")
 
+        return TestSuite("all samples", *tests)
 
-		for row in reader:
-			test = self._extract_test_from(row)
-    		tests.append(test)
 
 
     def _extract_test_from(self, element):
-    	identifier = element.get(self.URL_LABEL)
-    	error_percentage = element.get(self.ERR_PERCENTAGE)
-    	if error_percentage == 100:
-    		message = "No successful call to sample"
-    		return ErroneousTest(identifier, message)
+        identifier = element.get(self.URL_LABEL)
+        error_percentage = element.get(self.ERR_PERCENTAGE)
+        if error_percentage == 100:
+            message = "No successful call to sample"
+            #print 'test ' + identifier + ' is erroneaous with ' + str(error_percentage) + '%'
+            return ErroneousTest(identifier, message)
 
-		if error_percentage > 0:
-			message = error_percentage + "% of errors calling the sample"
-			return FailedTest(identifier, message)
-
+        if error_percentage > 0:
+            message = error_percentage + "% of errors calling the sample"
+            #print 'test ' + identifier + ' is failing with ' + str(error_percentage) + '%'
+            return FailedTest(identifier, message)
+		
         return SuccessfulTest(identifier)
 
 class JMeterCSVInvalidReport(Exception):
--- tests/execute/reporting/test_jmeter.py
+++ tests/execute/reporting/test_jmeter.py
@@ -17,10 +17,7 @@ from camp.execute.reporting.jmeter import JMeterCSVReader, \
 
 from unittest import TestCase
 
-import os
-
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-
+import StringIO
 
 class TheJMeterCSVReaderShould(TestCase):
 
@@ -31,52 +28,88 @@ class TheJMeterCSVReaderShould(TestCase):
 
 
     def test_raise_exception_when_given_empty_CSV_report(self):
-        csv_report = (
-        	"Label,# Samples,Average,Median,90\% Line,95\% Line,99\% Line,Min,Max,Error \%,Throughput,Received KB/sec,Sent KB/sec"
-        	)
-
+        csv_report = """Label,# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Error %,Throughput,Received KB/sec,Sent KB/sec"""
 
         with self.assertRaises(JMeterCSVInvalidReport):
-            self._reader._extract_from_text(csv_report.splitlines())
+            self._reader._extract_from_text(StringIO.StringIO(csv_report))
+
+	    
 
     def test_raise_exception_when_given_empty_CSV_report_from_file(self):
     	
-        csv_report = os.path.join(THIS_DIR, os.pardir, 'data_folder/empty_jmeter_report.csv')
+        csv_report = open('tests/execute/reporting/data_folder/empty_jmeter_report.csv', 'r')
 
 
         with self.assertRaises(JMeterCSVInvalidReport):
             self._reader._extract_from_text(csv_report)
 
     def test_raise_exception_when_given_empty_CSV_report_no_label_column(self):
-        csv_report = (
-        	"# Samples,Average,Median,90\% Line,95\% Line,99\% Line,Min,Max,Error \%,Throughput,Received KB/sec,Sent KB/sec"
-        	)
-
+        csv_report = """# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Error %,Throughput,Received KB/sec,Sent KB/sec"""
 
         with self.assertRaises(JMeterCSVInvalidReport):
-            self._reader._extract_from_text(csv_report.splitlines())
+            self._reader._extract_from_text(StringIO.StringIO(csv_report))
 
     def test_raise_exception_when_given_invalid_CSV_report_no_error_percentage_column(self):
-        csv_report = (
-        	"Label,# Samples,Average,Median,90\% Line,95\% Line,99\% Line,Min,Max,Throughput,Received KB/sec,Sent KB/sec"
-        	)
-
+        csv_report = """Label,# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Throughput,Received KB/sec,Sent KB/sec"""
 
         with self.assertRaises(JMeterCSVInvalidReport):
-            self._reader._extract_from_text(csv_report.splitlines())
+            self._reader._extract_from_text(StringIO.StringIO(csv_report))
 
     def test_raise_exception_when_given_invalid_CSV_report(self):
-        csv_report = (
-        	"# Samples,Average,Median,90\% Line,95\% Line,99\% Line,Min,Max,Throughput,Received KB/sec,Sent KB/sec"
-        	)
-
+        csv_report = """# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Throughput,Received KB/sec,Sent KB/sec"""
 
         with self.assertRaises(JMeterCSVInvalidReport):
-            self._reader._extract_from_text(csv_report.splitlines())
+            self._reader._extract_from_text(StringIO.StringIO(csv_report))
 
     def test_raise_exception_when_given_invalid_CSV_report_from_file(self):
-        csv_report = os.path.join(THIS_DIR, os.pardir, 'data_folder/invalid_jmeter_report.csv')
+        csv_report = open('tests/execute/reporting/data_folder/invalid_jmeter_report.csv','r')
 
 
         with self.assertRaises(JMeterCSVInvalidReport):
             self._reader._extract_from_text(csv_report)
+
+    def test_extract_a_testsuite_from_CSV(self):
+
+        csv_report = """Label,# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Error %,Throughput,Received KB/sec,Sent KB/sec
+Open home page,13,4.9,4.5,4.6,4.7,5.0,2.1,5.1,0.0,37.16,2.58,6735.0
+Login,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8
+Access personal area,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,20.0,40.03,3.01,535.8
+Logout,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8"""
+
+        test_suite = self._reader._extract_from_text(StringIO.StringIO(csv_report))
+
+        self.assertIsInstance(test_suite, TestSuite)
+        self.assertEquals(4, test_suite.run_test_count)
+
+    def test_extract_a_successful_testsuite_from_CSV(self):
+
+        csv_report = """Label,# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Error %,Throughput,Received KB/sec,Sent KB/sec
+Open home page,13,4.9,4.5,4.6,4.7,5.0,2.1,5.1,0.0,37.16,2.58,6735.0
+Login,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8
+Access personal area,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8
+Logout,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8"""
+
+        test_suite = self._reader._extract_from_text(StringIO.StringIO(csv_report))
+
+        self.assertIsInstance(test_suite, TestSuite)
+        self.assertEquals(4, test_suite.run_test_count)
+        self.assertEquals(4, test_suite.passed_test_count)
+        self.assertEquals(0, test_suite.failed_test_count)
+        self.assertEquals(0, test_suite.erroneous_test_count)
+
+    def test_extract_a_testsuite_with_failed_tests_from_CSV(self):
+
+        csv_report = """Label,# Samples,Average,Median,90% Line,95% Line,99% Line,Min,Max,Error %,Throughput,Received KB/sec,Sent KB/sec
+Open home page,13,4.9,4.5,4.6,4.7,5.0,2.1,5.1,0.0,37.16,2.58,6735.0
+Login,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8
+Access personal area,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,27.4,40.03,3.01,535.8
+Logout,13,5.1,4.3,4.2,4.9,5.3,3.7,6.1,0.0,40.03,3.01,535.8"""
+
+        test_suite = self._reader._extract_from_text(StringIO.StringIO(csv_report))
+
+        self.assertIsInstance(test_suite, TestSuite)
+        self.assertEquals(4, test_suite.run_test_count)
+        self.assertEquals(3, test_suite.passed_test_count)
+        self.assertEquals(1, test_suite.failed_test_count)
+        self.assertEquals(0, test_suite.erroneous_test_count)
+
