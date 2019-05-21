f2beb14728a035dc72f9452ae6717b470e9361b5
Daniele Gagliardi <daniele.gagliardi@eng.it>
2019-05-21 18:46:13 +0200

    Simple Java web app with authentication and functions to store test cases and their executions

---
+++ samples/java-web/template/testman/src/test/java/eu/stamp/camp/samples/testman/domain/TCExecutionIntegrationTest.java
@@ -0,0 +1,11 @@
+package eu.stamp.camp.samples.testman.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = TCExecution.class)
+public class TCExecutionIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
---
+++ samples/java-web/template/testman/src/test/java/eu/stamp/camp/samples/testman/domain/TCIntegrationTest.java
@@ -0,0 +1,11 @@
+package eu.stamp.camp.samples.testman.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = TC.class)
+public class TCIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
