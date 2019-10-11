d7d1175cd54b46b2abcb452107836a077a18f6c3
fernando <fernando.mendez@atos.net>
2019-10-11 18:10:01 +0200

    change port to -->8003

--- samples/stamp/atos/performance/template/citygo/entrypoint.sh
+++ samples/stamp/atos/performance/template/citygo/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 python manage.py migrate --no-input
+python manage.py makemigrations --no-input
 python manage.py collectstatic --no-input
 
 gunicorn citygo_settings.wsgi:application -b 0.0.0.0:8003 --workers 3
@@ -9,4 +10,3 @@ gunicorn citygo_settings.wsgi:application -b 0.0.0.0:8003 --workers 3
 #coverage html
 export BROWSER=firefox
 exec "$@"
-
