4faad98d4039c567313d3fc0a24f1cda2dbcf8f8
fernando <fernando.mendez@atos.net>
2019-10-23 10:20:45 +0200

    Update template tree for use case scenario 2

--- samples/stamp/atos/functional/template/postgres/init-db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE citygo_malaga;
-CREATE USER citygo WITH PASSWORD '5X6sdoq0!?az=v2aSX';
-ALTER ROLE citygo SET client_encoding TO 'utf8';
-ALTER ROLE citygo SET default_transaction_isolation TO 'read committed';
-ALTER ROLE citygo SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE citygo_malaga TO citygo;
-\q
-Exit
