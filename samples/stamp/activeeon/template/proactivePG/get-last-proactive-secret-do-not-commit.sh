5f232a6e617f3209beb24d9d9dc636a4b49bd44a
Franck Chauvel <franck.chauvel@gmail.com>
2019-11-07 12:24:18 +0100

    Move 'samples/stamp' to 'https://github.com/STAMP-project/camp-samples' (Issue #71)

--- samples/stamp/activeeon/template/proactivePG/get-last-proactive-secret-do-not-commit.sh
+++
@@ -1,1 +0,0 @@
-curl --silent --show-error http://mboussaa:<YOUR_TOKEN>@jenkins.activeeon.com/view/NightlyRelease/job/nightly-release/lastSuccessfulBuild/artifact/build/distributions/activeeon_enterprise-pca_server-linux-x64-8.5.0-SNAPSHOT.zip > /root/proactive.zip
